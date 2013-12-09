1c96dff0b84f92cc2642832f7348361e486fb8ee
VistaPOWA <hegyi.mokka@gmail.com>
2013-12-09 19:11:07 +0100

    initial commit

---
+++ CrossingSimulation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossingSimulation
+{
+    class Program
+    {
+        public struct Car
+        {
+            int inTime;
+            int outTime;
+            int position;
+            public Car(int a, int b, int c)
+            {
+                inTime = a;
+                outTime = b;
+                position = c;
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
